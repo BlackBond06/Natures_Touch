09b1435061e63043d5d73bdd7055f4c82d5c1ee6
BlackBond06 <eleazuosita007@yahoo.com>
2024-05-09 21:08:04 +0100

    added heroImageSlider component to main

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import HeroImageSlider from "../components/HeroImageSlider/HeroImageSlider";
+
+export default function Home() {
+  return (
+    <main className="relative w-full">
+      <HeroImageSlider />
+    </main>
+  );
+}
